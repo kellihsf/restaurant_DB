fc9237d7f0c21b74aed91d55d3710e1d818bfb58
Kelli <kellistone217@gmail.com>
2022-02-27 18:43:25 -0700

    Added routes for creating a new restaurant and getting all restaurants

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 const Sequelize = require("sequelize")
 
-const express = require("express")
+const { Restaurant} = require("./models")
+const express = require("express");
 const app = express()
 const port = 3000;
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+// GET all Restaurants 
+app.get("/restaurants", async (req, res) => {
+  const restaurants = await Restaurant.findAll({
+  });
+  res.json(restaurants);
+});
+
+// Get a single restaurant by ID
+app.get("/restaurants/:id", async (req, res) => {
+ try { 
+  const restaurantID = req.params.id; 
+  const oneRestaurant = await Restaurant.findbyPk(restaurantID)
+  res.json(oneRestaurant);
+ } catch (event) {
+   console.log(event)
+   res.status(404).json({
+     message: "Restaurant not found",
+   })
+ }
+})
+
+// Create new restaurant 
+app.post("/restaurants", async (req, res) => {
+  // req.body contains an Object with firstName, lastName, email
+  const { name, address, category } = req.body;
+  const newRestaurant = await Restaurant.create({
+    name,
+    address,
+    category,
+  });
+   // Send back the new restaurant's ID in the response:
+   res.json({
+    id: newRestaurant.id,
+  });
+});
+
+  
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
